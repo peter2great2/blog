60134ef6282d25b3a0d62f20ba17f973c35e312e
peter2great2 <peter2great2@gmail.com>
2023-05-05 05:43:04 +0100

    worked on the colors of the toggler

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const tContainer = document.getElementById("t-container");
 const circleOne = document.getElementById("circle-one");
 const circleTwo = document.getElementById("circle-two");
+
+circleTwo.addEventListener("click", () => {
+  tContainer.style.backgroundColor = "white";
+  circleOne.style.backgroundColor = "white";
+  circleTwo.style.backgroundColor = "black";
+});
+circleOne.addEventListener("click", () => {
+  tContainer.style.backgroundColor = "rgb(32, 31, 31)";
+  circleOne.style.backgroundColor = "white";
+  circleTwo.style.backgroundColor = "rgb(32, 31, 31)";
+});
