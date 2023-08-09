42714ad27b80dee1eaf66441b55ac10b1a2617af
peter2great2 <peter2great2@gmail.com>
2023-08-09 15:51:35 +0100

    worked on the date and the time

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let secondsTime = date.getSeconds();
 hour.innerHTML = hourTime;
 minutes.innerHTML = minutesTime;
 seconds.innerHTML = secondsTime;
+ampm.innerHTML = hour < 12 ? (ampm.innerHTML = "am") : (ampm.innerHTML = "pm");
 
 function updateSeconds() {
   secondsTime++;
@@ -58,3 +59,63 @@ setInterval(timeAndDate, 1000);
 // Update every second
 
 console.log("hello world");
+
+// this code will work in the date
+
+let day = document.getElementById("day");
+let dayTwo = document.getElementById("dayTwo");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+
+let dayTime = date.getDay();
+if (dayTime === 0) {
+  day.innerHTML = "Sunday, ";
+} else if (dayTime === 1) {
+  day.innerHTML = "Monday, ";
+} else if (dayTime === 2) {
+  day.innerHTML = "Tuesday, ";
+} else if (dayTime === 3) {
+  day.innerHTML = "Wednesday, ";
+} else if (dayTime === 4) {
+  day.innerHTML = "Thursday, ";
+} else if (dayTime === 5) {
+  day.innerHTML = "Friday, ";
+} else {
+  day.innerHTML = "Saturday, ";
+}
+
+let dayTwoTime = date.getDate();
+if (dayTwo.innerHTML < 10) {
+  dayTwo.innerHTML = `0${dayTwoTime}`;
+}
+
+let monthTime = date.getMonth();
+if (monthTime === 0) {
+  month.innerHTML = " January";
+} else if (monthTime === 1) {
+  month.innerHTML = " February";
+} else if (monthTime === 2) {
+  month.innerHTML = " March";
+} else if (monthTime === 3) {
+  month.innerHTML = " April";
+} else if (monthTime === 4) {
+  month.innerHTML = " May";
+} else if (monthTime === 5) {
+  month.innerHTML = " June";
+} else if (monthTime === 6) {
+  month.innerHTML = " July";
+} else if (monthTime === 7) {
+  month.innerHTML = " August";
+} else if (monthTime === 8) {
+  month.innerHTML = " September";
+} else if (monthTime === 9) {
+  month.innerHTML = " October";
+} else if (monthTime === 10) {
+  month.innerHTML = " November";
+} else if (monthTime === 11) {
+  month.innerHTML = " December";
+}
+
+let yearTime = date.getFullYear();
+year.innerHTML = ` ${yearTime}`;
+console.log(monthTime);
